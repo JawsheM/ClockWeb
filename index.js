80d5d4c3b54f7cd2e5f69354dc1b7953c2029db0
Joshua Melchor <melchorjosh4@gmail.com>
2023-10-31 13:30:55 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let hrs = document.getElementById("Hrs");
+let min = document.getElementById("Min");
+let sec = document.getElementById("Sec");
+
+setInterval(()=>{
+    let currentTime = new Date();
+
+    hrs.innerHTML  = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML  = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML  = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000)
